69fb4a4a3c1099eef4d30933b853090b14018c48
arjun <arjun@mac001ec20d778f.myhome.westell.com>
2008-04-17 19:24:49 -0400

    add csv output, support for multiple data_groups and data_sets

--- bin/parse_affy_chp.rb
+++ bin/parse_affy_chp.rb
@@ -3,7 +3,8 @@ $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 
 require 'affymetrix'
 
-chp = CHP.new("../data/NA06985_GW6_C.birdseed.chp", 10)
+chp = CHP.new("../data/NA06985_GW6_C.birdseed.chp")
+chp.write_data_sets('out.csv')
 
 # todo: other CHP files
 # chp2 = CHP.new("../data/HG-Focus-2-121502.calvin.CHP", 10)
