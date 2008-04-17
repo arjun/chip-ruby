9207e04dd1f790acba21936e5f14321adfc8363d
arjun <arjun@mac001ec20d778f.myhome.westell.com>
2008-04-17 15:22:46 -0400

    rm old cel files, more work on parent file headers and parameters

--- bin/parse_affy_chp.rb
+++ bin/parse_affy_chp.rb
@@ -1,7 +1,13 @@
-#!/usr/bin/env ruby
+# !/usr/bin/env ruby
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 
 require 'affymetrix'
 
 chp = CHP.new("../data/NA06985_GW6_C.birdseed.chp", 10)
-# chp2 = CHP.new("../data/10K_1-calvin.CHP", 10)
+
+# todo: other CHP files
+# chp2 = CHP.new("../data/HG-Focus-2-121502.calvin.CHP", 10)
+
+# todo: CEL files
+# cel = CEL.new("../data/Test3-1-121502.calvin.CEL", 10)
+
